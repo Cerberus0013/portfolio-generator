9cef7ec25b0d0bdb33d91deafaf68876a0885481
Breedlove13 <b.breedlove@outlook.com>
2021-03-28 21:25:20 -0700

    questions added

--- app.js
+++ app.js
@@ -1,15 +1,107 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require("inquirer");
 
+const promptProject = portfolioData => {
+  //if theres no 'projects array property create one
+  if(!portfolioData.projects){
+  portfolioData.projects = []
+  }
+}
+.then(projectData => {
+  portfolioData.projects.push(projectData);
+  if (projectData.confirmAddProject) {
+    return promptProject(portfolioData);
+  } else {
+    return portfolioData;
+  }
+});
 
+// const fs = require('fs')
+// const generatePage = require('./src/page-template.js');
+// ;
 
-const [name, github] = profileDataArgs
+// const pageHTML = generatePage(name, github)
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
 
+//   console.log("Portfolio Complete! check out index.html to see the output!");
+// })
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+promptUser().then((answers) => console.log(answers))
 
-  console.log("Portfolio Complete! check out index.html to see the output!");
-})
+const promptProject = () => {
+console.log(`
+  =======================
+  Add a New Project
+  ======================
+`);
+return inquirer.prompt([
+  {
+    type: "imput",
+    name: "name",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of the project (Required)",
+  },
+  {
+    type: "checkbox",
+    name: "languages",
+    message: "What did you build this project with? (check all that apply)",
+    choices: [
+      "Javascript",
+      "HTML",
+      "CSS",
+      "ES6",
+      "jQuery",
+      "Bootstrap",
+      "Node",
+    ],
+  },
+  {
+    type: "input",
+    name: "link",
+    message: "Enter the Gihub link to you project (Required)",
+  },
+  {
+    type: "confirm",
+    name: "feature",
+    message: "Would you like to feature this project?",
+    default: false,
+  },
+  {
+    type: "confirm",
+    name: "confirmAddProject",
+    message: "Would you like to enter another project?",
+    default: false,
+  },
+]);
+
+}
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
+});
